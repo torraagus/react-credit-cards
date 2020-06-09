90dc9ee559695b448051f99143120c953c603107
Torra Agustin <torra.agustinignacio@gmail.com>
2020-06-09 16:34:15 -0300

    update App component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+// import logo from './logo.svg';
+import Cards from "react-credit-cards";
+import "react-credit-cards/es/styles-compiled.css";
+import "./App.css";
 
 function App() {
+  const [number, setNumber] = useState("");
+  const [name, setName] = useState("");
+  const [expiry, setExpiry] = useState("");
+  const [cvc, setCvc] = useState("");
+  const [focus, setFocus] = useState("");
+
   return (
     <div className="App">
-      <header className="App-header">
+      <Cards
+        number={number}
+        name={name}
+        xpiry={expiry}
+        cvc={cvc}
+        focused={focus}
+      />
+      <form>
+        <input
+          type="tel"
+          name="number"
+          placeholder="Card Number"
+          value={number}
+          onChange={(e) => setNumber(e.target.value)}
+          onFocus={(e) => setFocus(e.target.name)}
+        />
+        <input
+          type="text"
+          name="name"
+          placeholder="Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          onFocus={(e) => setFocus(e.target.name)}
+        />
+        <input
+          type="text"
+          name="expiry"
+          placeholder="MM/YY Expiry"
+          value={expiry}
+          onChange={(e) => setExpiry(e.target.value)}
+          onFocus={(e) => setFocus(e.target.name)}
+        />
+        <input
+          type="tel"
+          name="cvc"
+          placeholder="CVC"
+          value={cvc}
+          onChange={(e) => setCvc(e.target.value)}
+          onFocus={(e) => setFocus(e.target.name)}
+        />
+      </form>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,7 +67,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
